4e99f066be533a0e444c72368fb88a41ecdc3224
Vu Motions <vulq.dev@gmail.com>
2025-05-26 23:39:56 +0700

    fix: bulid

--- src/app/[locale]/(main)/settings/(profile)/page.tsx
+++ src/app/[locale]/(main)/settings/(profile)/page.tsx
@@ -1,15 +1,13 @@
 import ProfileForm from './form-profile'
-import { Separator } from "@/components/ui/separator"
+import { Separator } from '~/components/ui/separator'
 
 export default function SettingsProfilePage() {
   return (
     <div>
-      <div className="space-y-6">
+      <div className='space-y-6'>
         <div>
-          <h3 className="text-lg font-medium">Thông tin cá nhân</h3>
-          <p className="text-sm text-muted-foreground">
-            Cập nhật thông tin cá nhân của bạn.
-          </p>
+          <h3 className='text-lg font-medium'>Thông tin cá nhân</h3>
+          <p className='text-muted-foreground text-sm'>Cập nhật thông tin cá nhân của bạn.</p>
         </div>
         <Separator />
         <ProfileForm />
