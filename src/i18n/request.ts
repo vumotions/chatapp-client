17f008ce3e69610a1c41590aa15ae323b4e6b2a8
Vu Motions <vulq.dev@gmail.com>
2025-05-31 15:01:23 +0700

    fix: file locale langs

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,5 +1,3 @@
-import fs from 'fs'
-import path from 'path'
 import { hasLocale } from 'next-intl'
 import { getRequestConfig } from 'next-intl/server'
 import { routing } from './routing'
@@ -7,19 +5,8 @@ import { routing } from './routing'
 export default getRequestConfig(async ({ requestLocale }) => {
   const requested = await requestLocale
   const locale = hasLocale(routing.locales, requested) ? requested : routing.defaultLocale
-
-  const messagesDir = path.join(process.cwd(), 'src', 'messages', locale)
-
-  const files = fs.readdirSync(messagesDir).filter((file) => file.endsWith('.json'))
-  const fileContents = files.map((file) => {
-    const content = fs.readFileSync(path.join(messagesDir, file), 'utf-8')
-    return JSON.parse(content)
-  })
-
-  const mergedFilesLocale = Object.assign({}, ...fileContents)
-
   return {
     locale,
-    messages: mergedFilesLocale
+    messages: (await import(`../../messages/${locale}.json`)).default
   }
 })
